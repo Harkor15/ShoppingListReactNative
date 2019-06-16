c0c4a0b987a2bfa1c86db3d172d0ed86400f1847
Harkor <krzbigos@gmail.com>
2019-06-16 23:34:03 +0200

    Done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View, ScrollView, Alert } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 import firebase from 'react-native-firebase';
 import LoginScreen from './screens/LoginScreen';
@@ -7,11 +7,83 @@ import RegisterScreen from './screens/RegisterScreen';
 import MainScreen from './screens/MainScreen';
 import DetailsScreen from './screens/DetailsScreen';
 import AddNewScreen from './screens/AddNewScreen';
+import AsyncStorage from 'react-native';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {};
+  }
+
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners();
+  }
+
+  componentWillUnmount() {
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        console.log("sld token", fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  }
+
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      this.getToken();
+    } catch (error) {
+      console.log('permission rejected');
+    }
+  }
+
+  async createNotificationListeners() {
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+        const { title, body } = notification;
+        this.showAlert(title, body);
+    });
+  
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+        const { title, body } = notificationOpen.notification;
+        this.showAlert(title, body);
+    });
+  
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+        const { title, body } = notificationOpen.notification;
+        this.showAlert(title, body);
+    }
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      console.log(JSON.stringify(message));
+    });
+  }
+  
+  showAlert(title, body) {
+    Alert.alert(
+      title, body,
+      [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false },
+    );
+  }
+
+
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
   }
 
   render() {
@@ -37,12 +109,12 @@ export default class App extends React.Component {
             key="detailsScreen"
             component={DetailsScreen}
             title="Details"
-          /> 
+          />
           <Scene
             key="addNewScreen"
             component={AddNewScreen}
             title="Add new list"
-            
+
           />
         </Scene>
       </Router>
